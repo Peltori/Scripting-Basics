39dc094d002ca7d1b49c1576a1976c2e2f0dfaa8
Petri Peltomaa <AC7995@student.labranet.jamk.fi>
2023-10-30 16:54:04 +0200

    Lisätty powershell tehtävä 2 ja kuva

---
+++ powershell/powershell-1/tehtava2.ps1
@@ -0,0 +1,4 @@
+﻿$firstname = Read-Host "Give your firstname please"
+$lastname = Read-Host "Give your lastname please"
+$groupcode = Read-Host "Give your groupcode please eg: AB1234"
+Write-Host "Welcome to the course: $firstname $lastname $groupcode"
