7adef581ddf61e5c955be05b0d3bea04b2bb01b2
Petri Peltomaa <AC7995@student.labranet.jamk.fi>
2023-10-30 17:07:14 +0200

    Lisätty powershell tehtävä 3 ja kuva

---
+++ powershell/powershell-1/tehtava3.ps1
@@ -0,0 +1,14 @@
+﻿# ask 2 integers from the user
+[int]$number1 = Read-Host "Give first integer please"
+[int]$number2 = Read-Host "Give second integer please"
+
+# Return addition, substraction, multiplication and division to the user
+$addition = $number1 + $number2
+$substraction = $number1 - $number2
+$multiplication = $number1 * $number2
+$division = $number1 / $number2
+
+Write-Host "Sum is: $addition"
+Write-Host "Substraction is: $substraction"
+Write-Host "Multiplication is: $multiplication"
+Write-Host "Division is: $division"
