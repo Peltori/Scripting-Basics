8ba51d8f25ec33d01673a42c51f99534fc9bceb1
Petri Peltomaa <AC7995@student.labranet.jamk.fi>
2023-11-06 16:31:29 +0200

    Lisätty Harj2 Tehtävä 5 ja kuva

---
+++ powershell/powershell-2/Tehtava5.ps1
@@ -0,0 +1,15 @@
+﻿param (
+  [string]$ip = '192.168.1.1',
+  [int]$tries = 1
+  )
+
+Write-Host "I will try to ping" $ip $tries "time, is this okay?"
+$perm = Read-Host "[Y/n]"
+
+if ($perm -like "Y") {
+    for ($i = 0; $i -lt $tries; $i++) {
+        Write-Output("try " + ($i +1) + " to ping: " + $ip)
+    }
+} else {
+    Exit
+}
