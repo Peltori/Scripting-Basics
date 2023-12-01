a7bab6ecea19fcdee32097d0d3c6923c8db7c9c4
Petri Peltomaa <AC7995@student.labranet.jamk.fi>
2023-12-01 12:25:56 +0200

    Lisätty Harj5 Teht5

---
+++ powershell/powershell-5/tehtava5.ps1
@@ -0,0 +1,23 @@
+﻿param(
+[string]$filename = 'localUsers.csv'
+)
+
+function Add-MyEvent {
+    param (
+        [string]$Type,
+        [string]$Message
+    )
+    
+    $source = "MyPowerShell"
+    Write-EventLog -LogName Application -Source $source -EntryType $Type -EventId 1 -Message $Message
+}
+
+try {
+    $users = Get-LocalUser | Select-Object -Property Name, Fullname, SID, LastLogon | Export-Csv -Path $filename -Delimiter ';'
+    Add-MyEvent -Type "Information" -Message "CSV file creation successfull: $filename"
+}
+catch {
+    Write-Host "$_"
+    Add-MyEvent -Type "Error" -Message "Failed to create CSV file: $filename"
+}
+notepad $filename
